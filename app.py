27e5cee8666dc693760620b1c3def4651dbaba5d
ANURAG VERMA <152633495+Anurag9140@users.noreply.github.com>
2025-03-23 23:56:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -131,7 +131,7 @@ cities = ['Hyderabad', 'Bangalore', 'Mumbai', 'Indore', 'Kolkata', 'Delhi',
        'Visakhapatnam', 'Pune', 'Raipur', 'Ranchi', 'Abu Dhabi',
        'Sharjah', 'Mohali', 'Bengaluru']
 
-pipe = pickle.load(open('Training/pipe.pkl','rb'))
+
 st.title('IPL Win Predictor')
 
 col1, col2 = st.columns(2)
